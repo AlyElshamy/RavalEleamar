440766970c98229a7f45a6e4acb016902398d4bd
alyelshamy <alielshamy900@gmail.com>
2023-01-18 23:39:48 +0300

    changes

---
+++ RavalElevators/RavalElevators/Areas/Site/Pages/Product.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RavalElevators.Areas.Home.Pages
+{
+    public class ProductModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
