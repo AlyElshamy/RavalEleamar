a36675e124231d9a72a00696d6c59200f6c45e77
alyelshamy <alielshamy900@gmail.com>
2023-01-19 19:19:08 +0300

    products images

---
+++ RavalElevators/RavalElevators/Areas/Site/Pages/silver.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RavalElevators.Areas.Site.Pages
+{
+    public class silverModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
